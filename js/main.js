d10c4faa56ede1c741e4b7807d2e92b4767ff6dd
Mystikal91 <mystikal91@gmail.com>
2024-11-24 01:12:52 +0100

    Aggiunta Manuali URBI

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready( function () {
+    $('.datatables-table').DataTable({
+        language: {
+            url: './vendor/datatables/i18n/it-IT.json',
+        },
+    });
+} );
